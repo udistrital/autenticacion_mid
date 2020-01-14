a443bc7c1d9e6e27ce45eb7976225e32d8e712ba
Fabián Sánchez <fadarsaleeing@gmail.com>
2020-01-14 13:32:51 -0500

    update .drone

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,4 @@ set -e
 set -u
 set -o pipefail
 
-# if [ -n "${PARAMETER_STORE:-}" ]; then
-#   export SGA_MID__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/sga_mid/db/username --output text --query Parameter.Value)"
-#   export SGA_MID__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/sga_mid/db/password --output text --query Parameter.Value)"
-# fi
-
 exec ./main "$@"
