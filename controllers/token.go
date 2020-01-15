4f30f2f3da9d3336d9a19860d56f4daa0a4addf8
Fabián Sánchez <fadarsaleeing@gmail.com>
2020-01-14 21:37:13 -0500

    En compañia de @jdc996 se modifica el servicio para la obtencion de roles y se corrige la respuesta en el envio de errores

--- controllers/token.go
+++ controllers/token.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/astaxie/beego"
 	"github.com/udistrital/autenticacion_mid/models"
-	"github.com/udistrital/utils_oas/formatdata"
 )
 
 // TokenController operations for Token
@@ -24,7 +23,7 @@ func (c *TokenController) URLMapping() {
 // @Description Recibe el correo electrónico del usuario desde la autenticación
 // @Param	email		path 	string	true		"The key for staticblock"
 // @Success 200 {object} models.Token
-// @Failure 403 is empty
+// @Failure 404 not found resource
 // @router /emailToken [post]
 func (c *TokenController) GetEmail() {
 	var v models.Token
@@ -46,52 +45,29 @@ func (c *TokenController) GetEmail() {
 // GetRol ...
 // @Title GetRol
 // @Description Recibe el usuario y devuelve el rol
-// @Param	email		path 	string	true		"The key for staticblock"
-// @Success 200 {object} models.RolesUsuario
-// @Failure 403 is empty
+// @Param	body		body 	  	true		"body for Concepto_nomina_por_persona content"
+// @Success 200 {object} models.Payload
+// @Failure 404 not found resource
 // @router /userRol [post]
 func (c *TokenController) GetRol() {
 	var (
-		v   models.Token
-		res map[string]interface{}
+		v string
 	)
-	res = make(map[string]interface{}, 0)
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
 		beego.Info("V: ", v)
-		if response, err := models.GetRolesByUser(&v); err == nil {
-			mapa1, _ := formatdata.ToMap(response, "json")
-			beego.Info(mapa1)
-			for k, v := range mapa1 {
-				res[k] = v
-			}
+		if response, err := models.GetRolesByUser(v); err == nil {
+			c.Data["json"] = response
 		} else {
-			fmt.Println(err)
+			fmt.Println("error: ", err)
 			c.Data["system"] = err
+			c.Abort("400")
 		}
 	} else {
-		fmt.Println(err)
+		fmt.Println("error: ", err)
 		c.Data["system"] = err
+		c.Abort("400")
 	}
 
-	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
-		beego.Info("V: ", v)
-		if response, err := models.GetInfoByEmail(&v); err == nil {
-			mapa2, _ := formatdata.ToMap(response, "json")
-			for k, v := range mapa2 {
-
-				res[k] = v
-				fmt.Println("res: ", res[k], ": ", v)
-			}
-
-		} else {
-			fmt.Println(err)
-			c.Data["system"] = err
-		}
-	} else {
-		fmt.Println(err)
-		c.Data["system"] = err
-	}
-	c.Data["json"] = res
 	c.ServeJSON()
 
 }
